0700a246693c4fa73373ef53d2ffe63238d1ab75
t-cool <hiroki802@hotmail.com>
2024-02-22 07:14:16 +0900

    コメント追加

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-// チャット画面
-const log = document.getElementById('log');
-
-// 開始ボタン
-const startButton = document.getElementById('start');
-
-// 停止ボタン
-const stopButton = document.getElementById('stop');
-
-// 音声選択
-const voiceSelection = document.getElementById('voiceSelection');
-
-// 音声認識中かの判定フラグ
-let listening = false;
-let recognition;
+const log = document.getElementById('log');// チャット画面
+const startButton = document.getElementById('start');// 開始ボタン
+const stopButton = document.getElementById('stop');// 停止ボタン
+const voiceSelection = document.getElementById('voiceSelection');// 音声選択
+let listening = false;// 音声認識中かの判定
+let recognition; // 音声認識のための変数
 
 startButton.addEventListener('click', () => {
     if (listening) return;
