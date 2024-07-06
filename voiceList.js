5acd517c32116db0b8e7df6c1d949d35c44deb77
NOGUCHI HIROKI <t-cool@t-cool.lan>
2024-07-06 12:46:45 +0900

    UI調整

--- voiceList.js
+++ voiceList.js
@@ -7,7 +7,7 @@ window.speechSynthesis.onvoiceschanged = () => {
 function populateVoiceList() {
     const selectedLang = languageSelection.value;
     voices = window.speechSynthesis.getVoices().filter(voice => voice.lang.startsWith(selectedLang.split('-')[0]));
-    voiceSelection.innerHTML = voices.map((voice, index) => `<option value="${index}">${voice.name} (${voice.lang})</option>`).join('');
+    voiceSelection.innerHTML = voices.map((voice, index) => `<option value="${index}">${voice.name}</option>`).join('');
 }
 
 languageSelection.addEventListener('change', populateVoiceList);
